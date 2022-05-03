cbd4d0ab7e89a3062ed408024944bf186d0ff5ca
DannyPhung <dphung@presidio.com>
2022-05-03 12:08:07 -0500

    Fixed Upload UI

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -6,20 +6,12 @@ function Content(props: any) {
   return (
     <div className="modal">
       <div className="modalContainer">
-        <div className="titleCloseBtn">
-          <button
-            onClick={() => {
-              props.setOpenModal(false)
-            }}
-          >
-            X
-          </button>
-        </div>
-
         <div className="body">
           <ul>
             {props.content?.files.map((file: any, index: any) => (
-              <li key={index}>{file.name}</li>
+              <li key={index}>
+                <p className="file-name">{file.name}</p>
+              </li>
             ))}
           </ul>
         </div>
--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -7,19 +7,17 @@ export default function Upload(props) {
   const [selectedFiles, setSelectedFiles] = useState({
     files: [],
   })
-  
-  const [modalOpen, setModalOpen] = useState(false);
-  const fileInput = useRef(null)
 
-  
+  const [modalOpen, setModalOpen] = useState(false)
+  const fileInput = useRef(null)
 
   const changeHandler = (event: { target: { files: any } }) => {
     var files = event.target.files
     console.log(files)
     var filesArr = Array.prototype.slice.call(files)
     console.log(filesArr.length)
-    setSelectedFiles({ files: [ ...filesArr] });
-    setModalOpen(true);
+    setSelectedFiles({ files: [...filesArr] })
+    setModalOpen(true)
   }
 
   // async function signOut() {
@@ -36,38 +34,40 @@ export default function Upload(props) {
       <fieldset className="upload-form">
         <legend>sftp.stfu.com</legend>
         <div className="logo">&nbsp;</div>
-        <h2>Upload documents</h2>
 
-        <div className="input-container">
-          <div className="uploader ">
-            Pick from File Explorer
-            <input
-              type="file"
-              name="files"
-              ref={fileInput}
-              multiple
-              onChange={changeHandler}
-              style={{ display: 'none' }}
-            />
-            <button
-              className="upload-btn"
-              onClick={() => fileInput.current.click()}
-            >
-              Select
+        <div className="uploader-container">
+          <h2>Upload Documents</h2>
+
+          <div className="input-container">
+            <div className="uploader ">
+              Pick from File Explorer
+              <input
+                type="file"
+                name="files"
+                ref={fileInput}
+                multiple
+                onChange={changeHandler}
+                style={{ display: 'none' }}
+              />
+              <button
+                className="upload-btn"
+                onClick={() => fileInput.current.click()}
+              >
+                Select
+              </button>
+            </div>
+            {modalOpen && (
+              <Content content={selectedFiles} setOpenModal={setModalOpen} />
+            )}
+            <p>OR</p>
+
+            <button className="button-drag-drop" onClick={handleSubmission}>
+              Drag and drop
             </button>
+            {/* <button onClick={signOut}>Sign out</button> */}
           </div>
-          {modalOpen  && <Content content={selectedFiles} setOpenModal={setModalOpen}/>}
-          <p>OR</p>
-
-          <button className="button-drag-drop" onClick={handleSubmission}>
-            Drag and Drop
-          </button>
-          {/* <button onClick={signOut}>Sign out</button> */}
         </div>
-        
       </fieldset>
-
-      
     </div>
   )
 }
