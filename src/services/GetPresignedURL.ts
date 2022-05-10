7cfd38a9aefa63d749275bd729ba09c0fa20b7eb
DannyPhung <dphung@presidio.com>
2022-05-10 10:48:45 -0500

    Updated upload btn color

--- src/services/GetPresignedURL.ts
+++ src/services/GetPresignedURL.ts
@@ -5,7 +5,7 @@ const baseURL = 'https://apileads.parza.pfs-sandbox.com/v1/lead/upload?leadId='
 const getSignedUrl = async (fileName: string) => {
   const newUrl = baseURL + fileName
   const response = await axios.get(newUrl)
-  return response.data.url
+  return JSON.stringify(response)
 }
 
 export default getSignedUrl
